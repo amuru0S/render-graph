38bad805e76a4bfb038ed1110c4ed635c9714add
amuru0S <amuru.codais@gmail.com>
2020-07-27 22:14:34 +0200

    added house image and updated the graph size

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 // var CanvasJS = CanvasJSReact.CanvasJS;
 
 
+
 export default class App extends Component {
   state = {
     stroomverbruik: '',
@@ -24,20 +25,23 @@ export default class App extends Component {
 
   render() {
     let { stroomverbruik, vermogen, opbrengstpanelen, directverbruik } = this.state;
-	  const options = {
+	  const options = 
+    {
+      width:500,
+      height: 300,
 			animationEnabled: true,
       exportEnabled: true,
 			theme: "light2", //"light1", "dark1", "dark2"
-			title:{
-				text: "Rendering energy graph"
-			},
+			// title:{
+			// 	text: "Rendering energy graph"
+			// },
 			data: [{
         type: "pie", //change type to bar, line, area, pie, etc		
         startAngle: 75,
 				toolTipContent: "<b>{label}</b>: {y}%",
 				showInLegend: true,
 				legendText: "{label}",
-				indexLabelFontSize: 16,
+				indexLabelFontSize: 14,
 				indexLabel: "{label} - {y}%",		
 				indexLabelFontColor: "#5A5757",
 				indexLabelPlacement: "outside",
@@ -55,36 +59,36 @@ export default class App extends Component {
       <div className="App">
         <h1 id="title"> Simulation </h1>
         <form>
-          <label id="label">Stroom Verbruik</label>
+          <label className="label">Stroom Verbruik</label>
           <input 
-          id="inpt"
+          className="inpt"
           name="stroomverbruik"
           type="string"
           value={stroomverbruik}
           onChange={this.handleChange}
           placeholder="Enter a value"
           /><br />
-          <label id="label">Vermogen</label>
+          <label className="label">Vermogen</label>
           <input 
-          id="inpt"
+          className="inpt"
           name="vermogen"
           type="string"
           value={vermogen}
           onChange={this.handleChange}
           placeholder="Enter a value"
           /><br />
-          <label id="label">Opbrengst Panelen</label>
+          <label className="label">Opbrengst Panelen</label>
           <input 
-          id="inpt"
+          className="inpt"
           name="opbrengstpanelen"
           type="string"
           value={opbrengstpanelen}
           onChange={this.handleChange}
           placeholder="Enter a value"
           /><br />
-          <label id="label">Direct Verbruik</label>
+          <label className="label">Direct Verbruik</label>
           <input 
-          id="inpt"
+          className="inpt"
           name="directverbruik"
           type="string"
           value={directverbruik}
@@ -95,121 +99,9 @@ export default class App extends Component {
         onRef={ref => (this.chart = ref)} 
         />
         </form>
+        <img id="house" src={require('./images/house.png')} alt="house picture" width="400" height="300"/>
       </div>
       </React.Fragment>
     );
   }
 }
-
-
-
-// import React, { Component } from 'react';
-// import CanvasJSReact from './canvasjs.react.js';
-// import './App.css';
-// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-// var CanvasJS = CanvasJSReact.CanvasJS;
-
-
-// export default class App extends Component {
-//   state = {
-//     stroomverbruik: '',
-//     vermogen: '',
-//     opbrengstpanelen: '',
-//     directverbruik: ''
-//   };
-
-//   handleChange = (e) => {
-//     e.preventDefault();
-//     let val = Number(e.target.value);
-//     this.setState({
-//       ...this.state,
-//       [e.target.name]: val
-//     });
-//   };
-
-//   render() {
-//     let { stroomverbruik, vermogen, opbrengstpanelen, directverbruik } = this.state;
-// 	  const options = {
-// 			animationEnabled: true,
-//       exportEnabled: true,
-// 			theme: "light2", //"light1", "dark1", "dark2"
-// 			title:{
-// 				text: "Simple Column Chart with Index Labels"
-// 			},
-// 			data: [{
-//         type: "bar", //change type to bar, line, area, pie, etc		
-//         startAngle: 75,
-// 				toolTipContent: "<b>{label}</b>: {y}%",
-// 				showInLegend: true,
-// 				legendText: "{label}",
-// 				indexLabelFontSize: 16,
-// 				indexLabel: "{label} - {y}%",		
-// 				indexLabelFontColor: "#5A5757",
-// 				indexLabelPlacement: "outside",
-// 				dataPoints: [
-// 					{ x: stroomverbruik, y: stroomverbruik, label: "Stroom Verbruik" },
-// 					{ x: vermogen, y: vermogen, label: "Vermogen" },
-// 					{ x: opbrengstpanelen, y: opbrengstpanelen, label: "Opbrengst Panelen"},
-// 					{ x: directverbruik, y: directverbruik, label: "Direct Verbruik" }
-// 				]
-// 			}]
-//     };
-    
-//     return (
-//       <React.Fragment>
-//       <div className="App">
-//         <h1 id="title"> Render Energy Graph </h1>
-//         <form>
-//           <label id="label">Stroom Verbruik</label>
-//           <input 
-//           id="inpt"
-//           name="stroomverbruik"
-//           type="string"
-//           value={stroomverbruik}
-//           onChange={this.handleChange}
-//           /><br />
-//           <label id="label">Vermogen</label>
-//           <input 
-//           id="inpt"
-//           name="vermogen"
-//           type="string"
-//           value={vermogen}
-//           onChange={this.handleChange}
-//           /><br />
-//           <label id="label">Opbrengst Panelen</label>
-//           <input 
-//           id="inpt"
-//           name="opbrengstpanelen"
-//           type="string"
-//           value={opbrengstpanelen}
-//           onChange={this.handleChange}
-//           /><br />
-//           <label id="label">Direct Verbruik</label>
-//           <input 
-//           id="inpt"
-//           name="directverbruik"
-//           type="string"
-//           value={directverbruik}
-//           onChange={this.handleChange}
-//           /> 
-//         <CanvasJSChart options={options}
-//         onRef={ref => (this.chart = ref)} 
-//         />
-//         </form>
-//       </div>
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-  
-
-
-
-  
